8612948b934c8f78fe8ac7167caddf0f7cd07789
tristan <tvut@outlook.com>
2020-04-18 16:19:08 -0700

    first commit

---
+++ precache-manifest.f0978f1fd644c2b20f67ebe6b2853dce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "946346d302ca0a9cf7a33dd103326e02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e20826aa6491ce863a4",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "059cf09c92c834d5c14d",
+    "url": "/static/css/main.872a02ca.chunk.css"
+  },
+  {
+    "revision": "7e20826aa6491ce863a4",
+    "url": "/static/js/2.45133d5c.chunk.js"
+  },
+  {
+    "revision": "059cf09c92c834d5c14d",
+    "url": "/static/js/main.a485d5f4.chunk.js"
+  },
+  {
+    "revision": "e04c4d462f9c75b4f4c0",
+    "url": "/static/js/runtime-main.54d98075.js"
+  }
+]);
